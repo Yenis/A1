60d9d236bdb503221c372aae5e590ceb1c526037
Yenis <yenis92@live.com>
2016-09-25 15:17:04 +0200

    Update 06 ReturnSameBirthDate.sql

--- 06 ReturnSameBirthDate.sql
+++ 06 ReturnSameBirthDate.sql
@@ -1,3 +1,3 @@
-SELECT FirstName, LastName FROM employee_profile_data WHERE 
-  BirthDate IN (SELECT BirthDate FROM employee_profile_data GROUP BY BirthDate HAVING COUNT(*) > 1);
+SELECT * FROM employee_profile WHERE 
+  BirthDate IN (SELECT BirthDate FROM employee_profile GROUP BY BirthDate HAVING COUNT(*) > 1);
   
