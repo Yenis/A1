64eeb4aea6908eecd231a62f24ac238816a5f150
Yenis <yenis92@live.com>
2016-09-25 15:17:20 +0200

    Update 07 ReturnOfficialPhoneNumber.sql

--- 07 ReturnOfficialPhoneNumber.sql
+++ 07 ReturnOfficialPhoneNumber.sql
@@ -1,4 +1,2 @@
-SELECT FirstName, LastName FROM employee_profile_data WHERE 
-  ContactNumbers LIKE '%offic%' OR
-  ContactNumbers LIKE '%workpl%';
-  
+SELECT * FROM employee_contact_numbers WHERE 
+  WorkOROfficeNumber <>''
